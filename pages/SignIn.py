6caee5f4acd9b037b21f0122471ee57ed439d6fa
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-03-09 20:36:30 +0530

    Implemented SQL for Login and User Creation

--- pages/SignIn.py
+++ pages/SignIn.py
@@ -1,60 +1,86 @@
 import streamlit as st
-import toml
 import SessionState
 from passlib.hash import bcrypt
 import time
+import toml
+import mysql.connector
+
+# Load the MySQL connection details from secrets.toml
+mysql_config = {
+'user': 'mysql',
+'password': 'mysql',
+'host': '127.0.0.1',
+'port': '3306',
+'database': 'STOCKMANAGER',
+'raise_on_warnings': True,}
+
+# Establish a connection to the MySQL database
+#@st.cache_resource
+
+
+conn = mysql.connector.connect(**mysql_config)
+cursor = conn.cursor()
+conn.reconnect(mysql_config)
 
 def authenticate(username, password):
-    # Read usernames and hashed passwords from ./.streamlit/secrets.toml
-    secrets = toml.load("./.streamlit/secrets.toml")
-    users = secrets.get("users", {})
+    # Fetch the hashed password from the database
+    query = "SELECT Password FROM USER WHERE UID = %s"
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
 
-    # Check if the username exists and the password is correct
-    if username in users:
-        hashed_password = users[username]
+    if result:
+        hashed_password = result[0]
+        # Check if the provided password matches the hashed password
         if bcrypt.verify(password, hashed_password):
             return True
     return False
 
-def register(username, password):
-    # Read usernames and hashed passwords from ./.streamlit/secrets.toml
-    secrets = toml.load("./.streamlit/secrets.toml")
-    users = secrets.get("users", {})
-
+def register(username, password, name, email, phone_number):
     # Check if the username already exists
-    if username in users:
+    query = "SELECT * FROM USER WHERE UID = %s"
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
+
+    if result:
         return False
 
-    # Hash the password and save the new user
+    # Hash the password
     hashed_password = bcrypt.hash(password)
-    users[username] = hashed_password
 
-    # Update ./.streamlit/secrets.toml with the new user
-    secrets["users"] = users
-    with open("./.streamlit/secrets.toml", "w") as f:
-        toml.dump(secrets, f)
+    # Insert the new user into the database
+    query = "INSERT INTO USER (UID, Name, Email, Password, PhoneNumber) VALUES (%s, %s, %s, %s, %s)"
+    values = (username, name, email, hashed_password, phone_number)
+    cursor.execute(query, values)
+    conn.commit()
 
     return True
 
 def show(session_state: SessionState):
     st.title("Sign In / Register")
 
-   
-
     # Allow the user to choose between registering and logging in
-    if not session_state.option:
+    if  not session_state.option and  not session_state.register and not session_state.is_authenticated:
         session_state.option = st.button("Sign In")
         session_state.register = st.button("Register")
 
     if session_state.register:
         st.subheader("Register")
         reg_username = st.text_input("New Username")
+        if(reg_username == ''):
+            st.warning("Please enter a username")
         reg_password = st.text_input("New Password", type="password")
+        reg_name = st.text_input("Name")
+        reg_email = st.text_input("Email")
+        reg_phone_number = st.text_input("Phone Number")
         if st.button("Submit Registration"):
-            if register(reg_username, reg_password):
+            if register(reg_username, reg_password, reg_name, reg_email, reg_phone_number):
                 st.success("Registration successful!")
+                if(session_state.register):
+                    session_state.register = False
             else:
                 st.error("Username already exists!")
+                if(session_state.register):
+                    session_state.register = False
 
     elif session_state.option and not session_state.is_authenticated:
         st.subheader("Log In")
@@ -65,13 +91,15 @@ def show(session_state: SessionState):
                 session_state.is_authenticated = True
                 session_state.username = username
                 st.success("You have successfully logged in!")
+                if(session_state.option):
+                    session_state.option = False
                 with st.spinner("Redirecting..."):
                     time.sleep(5)
                     st.switch_page("./Home.py")
             else:
                 st.error("Invalid username or password")
-                
-     # If the user is already authenticated, show the logout button
+
+    # If the user is already authenticated, show the logout button
     if session_state.is_authenticated:
         if st.button("Logout"):
             session_state.is_authenticated = False
@@ -80,6 +108,19 @@ def show(session_state: SessionState):
             session_state.register = None
             st.success("You have been logged out!")
 
+    # Example of using st.connection to cache database queries
+   # Example of using st.connection to cache database queries
+    #conn = st.connection('mysql',type='sql')
+    cursor.execute("SELECT * FROM USER")
+    df = cursor.fetchall()
+    #print(df)
+    #for row in df:
+        #st.write(f"{row[0]} - {row[1]} - {row[2]} - {row[3]}")
+
 # Call the show() function to render the content
-session_state = SessionState.get(is_authenticated=False, option=None, register=None,username=None,)
+session_state = SessionState.get(is_authenticated=False, option=None, register=None, username=None)
 show(session_state)
+
+# Close the database connection
+print("closingConnection")
+conn.close()    
